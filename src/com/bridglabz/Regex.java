78d03cf90cedc976955030bf68fe6e1de0cc08e1
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-05-12 23:09:50 +0530

    Added: Checking if the entered email id is valid or not.

--- src/com/bridglabz/Regex.java
+++ src/com/bridglabz/Regex.java
@@ -5,37 +5,19 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Regex {
-    static Scanner scanner = new Scanner(System.in);
-    static Pattern pattern;
-    static Matcher matcher;
-    //static String testString;
-
-    private static void validFirstName() {
-        pattern = Pattern.compile("^[A-Z][a-z]{2,}$");
-        System.out.println("Enter First Name : ");
-        String firstName = scanner.nextLine();
-        matcher = pattern.matcher(firstName);
-        if (matcher.matches()) {
-            System.out.println("Valid");
-        } else {
-            System.out.println("Not-Valid");
-        }
-    }
-
-    private static void validLastName() {
-        pattern = Pattern.compile("^[A-Z][a-z]{2,}$");
-        System.out.println("Enter Last Name : ");
-        String lastName = scanner.nextLine();
-        matcher = pattern.matcher(lastName);
-        if (matcher.matches()) {
-            System.out.println("Valid");
-        } else {
-            System.out.println("Not-Valid");
-        }
-    }
+    static Pattern pattern = Pattern.compile("abc.[a-z]{0,3}@bl.co.[a-z]{0,2}$");
 
     public static void main(String[] args) {
-        validFirstName();
-        validLastName();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Email Id: ");
+        String testString = scanner.next();
+        Matcher matcher = pattern.matcher(testString);
+
+        if (matcher.matches()) {
+            System.out.println("Valid");
+
+        } else {
+            System.out.println("Not-Valid");
+        }
     }
 }
