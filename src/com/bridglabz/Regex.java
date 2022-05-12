6a1abb8400686694763cb7aa55f9435c9cf37fb4
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-05-12 22:31:38 +0530

    Added: Checking the fisrt name is valid or not.

--- src/com/bridglabz/Regex.java
+++ src/com/bridglabz/Regex.java
@@ -1,7 +1,24 @@
 package com.bridglabz;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Regex {
+    private static void validFirstName() {
+        Scanner scanner = new Scanner(System.in);
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}");
+        System.out.println("Enter First Name : ");
+        String testString = scanner.nextLine();
+        Matcher matcher = pattern.matcher(testString);
+        if (matcher.matches()) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Not-Valid");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Regex Programs.");
+        validFirstName();
     }
 }
